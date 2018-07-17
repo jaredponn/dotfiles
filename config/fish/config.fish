bc7a36b1bb3aa23ce3de8407c626734c8d64c418
jaredponn <jpon1234@gmail.com>
2018-07-16 23:42:52 -0600

    fixed the werid space shift

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,52 +1,56 @@
-# Start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
-end
-
-# Removes the Fish greeting
+## Start X at login
+#if status --is-login
+#    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+#        exec startx -- -keeptty
+#    end
+#end
+#
+## Removes the Fish greeting
 set fish_greeting
 
-# fish git prompt
+## fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡ '
-set __fish_git_prompt_char_stagedstate '→ '
-set __fish_git_prompt_char_stashstate '↩ '
-set __fish_git_prompt_char_upstream_ahead '↑ '
-set __fish_git_prompt_char_upstream_behind '↓ '
- 
-# Set the fish prompt
+## Status Chars
+#set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_dirtystate ' '
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+
+# git stuff
 function fish_prompt
-        set last_status $status
-        set_color $fish_color_cwd
-        printf '%s' (prompt_pwd)
-        set_color normal
-        printf '%s $ ' (__fish_git_prompt)
-       set_color normal
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
 end
 
-# fkill - kill process
+## fkill - kill process
 function fkill
-  set PROCESS (ps -ef | sed 1d | fzf -m | awk '{print $2}')
-  kill $PROCESS
-  echo "Killed " $PROCESS
+ set PROCESS (ps -ef | sed 1d | fzf -m | awk '{print $2}')
+ kill $PROCESS
+ echo "Killed " $PROCESS
 end
 
-# ALIASES
+## ALIASES
 alias v "vim" # v for vim
 alias n "nvim" # n for vim
 alias zathura "zathura --fork" # v for vim
-alias ghc "stack ghc --" 
-alias ghci "stack ghci" 
+alias ghc "stack ghc --"
+alias ghci "stack ghci"
 
-# modestly get yt song
+## modestly get yt song
 alias getsong "youtube-dl --extract-audio -o '~/Music/bin/%(title)s.%(ext)s' "
 
-# adding .local/bin to the path. Needed for Haskell's Stack
+## adding .local/bin to the path. Needed for Haskell's Stack
 set -gx PATH /home/jared/.local/bin $PATH
