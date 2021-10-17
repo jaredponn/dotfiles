3266220de2aaa5160d04e60020dcd5e25f8a7548
jared <>
2021-10-17 01:39:30 -0600

    configuration.nix

---
+++ sourcenixosrc.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Simple script for copying the configuration.nix over to the location it should be.
+sudo sh -c 'rm /etc/nixos/configuration.nix; cp ./configuration.nix /etc/nixos/configuration.nix'
